8490f9700cab1e87921277eb5ca2602151cb1372
Pete Gadomski <pete.gadomski@gmail.com>
2014-12-05 15:03:30 -0700

    Fix stuff up, get ready for the remake

--- plot-magnitude.R
+++ plot-magnitude.R
@@ -7,6 +7,6 @@ args <- commandArgs(trailingOnly = T)
 infile <- args[1]
 outfile <- args[2]
 
-p <- rutils::plot_magnitude(rutils::import_change(infile, header = TRUE),
+p <- rutils::plot_magnitude(rutils::import_change(infile, skip = 1, sep = ","),
                             low = "#56B1F7", high = "red", limits = c(3.5, 6))
 ggplot2::ggsave(outfile, p)
